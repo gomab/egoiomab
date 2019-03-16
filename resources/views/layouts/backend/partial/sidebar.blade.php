a64ea55cbaff43a82f69b619f6bcef1c01e0d1b0
gomab <egoiomab@gmail.com>
2019-03-16 08:48:54 +0100

    Admin Backend Layout

---
+++ resources/views/layouts/backend/partial/sidebar.blade.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gomab
+ * Date: 3/16/19
+ * Time: 8:00 AM
+ */
+?>
+
+<aside id="leftsidebar" class="sidebar">
+    <!-- User Info -->
+    <div class="user-info">
+        <div class="image">
+            <img src="{{ asset('assets/backend/images/user.png') }}" width="48" height="48" alt="User" />
+        </div>
+
+        <div class="info-container">
+            {{ session('status') }}
+            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><strong>{{ Auth::user()->name }}</strong></div>
+            <div class="email">{{ Auth::user()->email }} | {{ Auth::user()->role->name }}</div>
+            <div class="btn-group user-helper-dropdown">
+                <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                <ul class="dropdown-menu pull-right">
+                    <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="javascript:void(0);"><i class="material-icons">group</i>Followers</a></li>
+                    <li><a href="javascript:void(0);"><i class="material-icons">shopping_cart</i>Sales</a></li>
+                    <li><a href="javascript:void(0);"><i class="material-icons">favorite</i>Likes</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="javascript:void(0);"><i class="material-icons">input</i>Sign Out</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- #User Info -->
+    <!-- Menu -->
+    <div class="menu">
+        <ul class="list">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="active">
+                <a href="{{ route('admin.dashboard') }}">
+                    <i class="material-icons">home</i>
+                    <span>Home</span>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+    <!-- #Menu -->
+    <!-- Footer -->
+    <div class="legal">
+        <div class="copyright">
+            &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+        </div>
+        <div class="version">
+            <b>Version: </b> 1.0.5
+        </div>
+    </div>
+    <!-- #Footer -->
+</aside>
---
+++ resources/views/layouts/backend/partial/topbar.blade.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gomab
+ * Date: 3/16/19
+ * Time: 7:54 AM
+ */
+?>
+
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+            <a href="javascript:void(0);" class="bars"></a>
+            <a class="navbar-brand" href="index.html">ADMINBSB - MATERIAL DESIGN</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Call Search -->
+                <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
+                <!-- #END# Call Search -->
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
