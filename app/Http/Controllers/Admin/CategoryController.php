3285a274dcef0883bbad272ef16da2eb7309a539
gomab <egoiomab@gmail.com>
2019-04-15 20:42:56 +0200

    delete post

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -197,6 +197,22 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //return $category = Category::find($id);
+
+        $category = Category::find($id);
+
+        if(Storage::disk('public')->exists('category/'.$category->image)){
+            Storage::disk('public')->delete('category/'.$category->image);
+        }
+
+        if(Storage::disk('public')->exists('category/slider/'.$category->image)){
+            Storage::disk('public')->delete('category/slider/'.$category->image);
+        }
+
+        $category->delete();
+
+        Toastr::success('Categorie supprimée avec succes :)', 'Success');
+
+        return redirect()->back();
     }
 }
