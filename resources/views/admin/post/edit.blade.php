1d310c455ccef22fc99fed43fad468b9fe985d03
gomab <egoiomab@gmail.com>
2019-04-16 22:47:43 +0200

    Edit Post

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -10,54 +10,159 @@
 
 @extends('layouts.backend.app')
 
-@section('title', 'Category')
+@section('title', 'post')
 
 @push('css')
-
-
+    <!-- Bootstrap Select Css -->
+    <link href="{{ asset('assets/backend/plugins/bootstrap-select/css/bootstrap-select.css') }}" rel="stylesheet" />
 @endpush
 
 @section('content')
 
-    <!-- Vertical Layout | With Floating Label -->
-    <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="card">
-                <div class="header">
-                    <h2>
-                        MODIFIER Categorie
-                    </h2>
-                </div>
-                <div class="body">
-                    <form action="{{ route('admin.category.update', $category->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-group form-float">
-                            <div class="form-line">
-                                <input type="text" id="name" name="name" class="form-control" value="{{ $category->name }}">
-                                <label class="form-label">Category Name</label>
+    <div class="container-fuild">
+        <!-- Vertical Layout | With Floating Label -->
+        <form action="{{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row clearfix">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                MAJ Article
+                            </h2>
+                        </div>
+                        <div class="body">
+
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" id="name" name="title" class="form-control" value="{{ $post->title }}">
+                                    <label class="form-label">Titre de l'article</label>
+                                </div>
                             </div>
+
+                            <div class="form-group">
+                                <label for="image">Image de l'article</label>
+                                <input type="file" name="image">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="checkbox" id="publish" class="filled-in" name="status" value="1" {{ $post->status == true ? 'checked' : '' }}>
+                                <label for="publish">Publié</label>
+                            </div>
+
                         </div>
+                    </div>
+                </div>
 
-                        <div class="form-group">
-                            <input type="file" name="image">
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Catégories et Tags
+                            </h2>
                         </div>
+                        <div class="body">
 
-                        <br>
+                            <div class="form-group form-float">
+                                <div class="form-line {{ $errors->has('categories') ? 'focused error' : '' }}">
+                                    <label for="category">Catégorie</label>
+                                    <select name="categories[]" id="category" class="form-control show-tick" data-live-search="true" multiple>
+                                        @foreach($categories as $category)
+                                            <option
+                                                    @foreach($post->categories as $postCategory)
+                                                            {{ $postCategory->id == $category->id ? 'selected' : '' }}
+                                                    @endforeach
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
 
-                        <a href="{{ route('admin.category.index') }}" class="btn btn-danger m-t-15 waves-effect">Retour</a>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
 
-                        <button type="submit" class="btn btn-primary m-t-15 waves-effect">Enregistrer</button>
-                    </form>
+                            <div class="form-group form-float">
+                                <div class="form-line {{ $errors->has('tags') ? 'focused error' : '' }}">
+                                    <label for="tag">Tag</label>
+                                    <select name="tags[]" id="tag" class="form-control show-tick" data-live-search="true" multiple>
+                                        @foreach($tags as $tag)
+                                            <option
+                                                    @foreach($post->tags as $postTag)
+                                                    {{ $postTag->id == $tag->id ? 'selected' : '' }}
+                                                    @endforeach
+
+                                                    value="{{ $tag->id }}">{{ $tag->name }}</option>
+
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+
+
+                            <a href="{{ route('admin.post.index') }}" class="btn btn-danger m-t-15 waves-effect">Back</a>
+
+                            <button type="submit" class="btn btn-primary m-t-15 waves-effect">SUBMIT</button>
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Ecrire l'article...
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <textarea name="body" id="tinymce" cols="30" rows="10">
+                                {{ $post->body }}
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </form>
+        <!-- Vertical Layout | With Floating Label -->
     </div>
-    <!-- Vertical Layout | With Floating Label -->
 
 @endsection
 
 @push('js')
+    <!-- Select Plugin Js -->
+    <script src="{{ asset('assets/public/backend/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
+
+    <!-- TinyMCE -->
+    <script src="{{ asset('assets/backend/plugins/tinymce/tinymce.js') }}"></script>
+
+    <script>
+        $(function () {
+            //CKEditor
+            // CKEDITOR.replace('ckeditor');
+            //CKEDITOR.config.height = 300;
+
+            //TinyMCE
+            tinymce.init({
+                selector: "textarea#tinymce",
+                theme: "modern",
+                height: 300,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'emoticons template paste textcolor colorpicker textpattern imagetools'
+                ],
+                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                toolbar2: 'print preview media | forecolor backcolor emoticons',
+                image_advtab: true
+            });
+            tinymce.suffix = ".min";
+            tinyMCE.baseURL = '{{ asset('assets/backend/plugins/tinymce') }}';
+        });
+    </script>
 
 
 @endpush
