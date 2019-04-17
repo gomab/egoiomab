22e5b8e32571836b7d234fdf757fe964f49d13ff
gomab <egoiomab@gmail.com>
2019-04-17 12:39:25 +0200

    Delete Post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -195,6 +195,16 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if (Storage::disk('public')->exists('post/'.$post->image)){
+            Storage::disk('public')->delete('post/'.$post->image);
+        }
 
+        $post->categories()->detach();
+        $post->tags()->detach();
+        $post->delete();
+
+        Toastr::success('Article supprimé !!', 'Success');
+
+        return redirect()->back();
     }
 }
